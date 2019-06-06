26abd26d847b2cdc2af750d5f442d967aa7c5e45
Anup Kumar Gupta <anup@troopr.io>
2019-06-06 22:21:28 +0530

    build folder issue

--- server.js
+++ server.js
@@ -85,7 +85,7 @@ app.use('/api/request', requestRouter);
 if(process.env.NODE_ENV === 'production'){
     app.use(express.static("client/build"));
     app.get('*',(req,res) =>{
-        res.sendFile(path.resolve(__dirname,'client','builld','index.html'))
+        res.sendFile(path.resolve(__dirname,'client','build','index.html'))
     })
 }
 
